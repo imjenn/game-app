443da0d917568acff43f147778a213c7e57030d9
Jenn Nguyen <jennguy17@gmail.com>
2022-02-02 18:05:04 -0800

    fixed modal popup

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -4,47 +4,47 @@ import {useState} from "react";
 import style from './Chat.module.css'
 import io from "socket.io-client";
 import axios from "axios";
+import { useHistory } from 'react-router-dom';
 
 const Chat = (props) =>{
     const user = JSON.parse(localStorage.getItem("User"));
     const [roomURL, setRoomURL] = useState("");
     const [roomID, setRoomID] = useState("");
+    const [error, setError] = useState("");
 
     const joinRoom = (e) => {
         if (roomURL !== ""){
             joinRoomURL(e,roomURL)
             return;
         }
-
-        console.log("hit hit")
         let chatInfo ={
             user: user,
             roomID: roomID
         };
 
-        e.preventDefault();
+        // e.preventDefault();
         axios.post("http://localhost:8000/joinchat", chatInfo, { withCredentials: true })
             .then(res => {
-                console.log(res.data)
+                console.log(res.data);
+                window.location.reload();
             })
             .catch(err => {
-                console.log(err.response);
-                console.log(err.response.data)
+                console.log(err.response.data);
+                setError(err.response.data.msg);
             })
-
     }
 
-    const joinRoomURL = (e,url) => {
+    const joinRoomURL = (e, url) => {
         let chatInfo ={user: user};
         e.preventDefault();
         axios.post(url, chatInfo, { withCredentials: true })
             .then(res => {
                 console.log(res.data)
-                console.log("ney")
+                window.location.reload();
             })
             .catch(err => {
-                console.log(err.response);
                 console.log(err.response.data)
+                setError(err.response.data.msg);
             })
 
     }
@@ -60,29 +60,30 @@ const Chat = (props) =>{
                 <Modal.Body>
                     <div className={style.joinChatContainer}>
                         <div className={style.JoinChatBox}>
+                            { error && <p style={{color: "red"}}>{error}</p>}
                                 <input
                                     type="text"
                                     name={'url'}
                                     placeholder="URL...."
-                                    onChange={(event) => {
-                                        setRoomURL(event.target.value);
+                                    onChange={(e) => {
+                                        setRoomURL(e.target.value);
                                     }}
                                 /><br/>
                                 <span>OR</span><br/>
                                 <input
                                     type="text"
                                     placeholder="Room ID..."
-                                    onChange={(event) => {
-                                        setRoomID(event.target.value);
+                                    onChange={(e) => {
+                                        setRoomID(e.target.value);
                                     }}
-                                /><br/><br/>
+                                />
                         </div>
                     </div>
                 </Modal.Body>
 
                 <Modal.Footer>
                     <Button variant="secondary" onClick={props.handleClose}>Close</Button>
-                    <Button variant="primary" onClick={joinRoom}>Save changes</Button>
+                    <Button variant="primary" onClick={joinRoom}>Join Room</Button>
                 </Modal.Footer>
             </Modal.Dialog>
         </div>
