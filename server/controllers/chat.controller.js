036fbf2944fcbcd2efe1e19e781b94926171635f
Oscar Vaquera <vaqueraoscar0@gmail.com>
2022-02-03 13:56:42 -0800

    Save/receive message functionality works for every chat room

--- server/controllers/chat.controller.js
+++ server/controllers/chat.controller.js
@@ -31,6 +31,60 @@ module.exports = {
             .catch(err => res.status(400).json({msg: "not found", err}))
 
     },
+    createChat: async (req, res) => {
+        console.log(req.body)
+
+
+        Chat.exists({ id: req.body.id, roomName: req.body.roomName })
+            .then(chatExists => {
+                if (chatExists) {
+                    return Promise.reject({
+                        errors: { 'duplicate': "Chat already exists" }
+                    })
+                }
+                // If user does not exist, create new user
+                const chatRoom = new Chat(req.body);
+                return chatRoom.save();
+            })
+            .then((chat) => {
+                res.json({ msg: "Success", "chat": chat })
+            })
+            .catch(err => {
+                res.status(400).json(err)
+            })
+    },
+
+    newMessageList: async (req, res) => {
+        //console.log(req.body)
+
+        Chat.findOneAndUpdate(
+            {
+                _id: req.body.roomID,
+            }, {$push: {
+                    messageHistory: [{
+                        username: req.body.author,
+                        msg: req.body.message,
+                        timestamp: req.body.time
+                    }]
+                }}, function (err, res){
+                //console.log(err);
+            }
+        )
+    },
+
+    messageList: async (req, res) => {
+        console.log(req.body)
+
+        Chat.findOne({_id:  '61eaf59079904068e8a661dd' })
+            .then( user => {
+                if (user === null) {
+                    res.status(400).json({msg: "User not found"});
+                } else {
+                    //console.log(user.messageHistory)
+                    res.send(user);
+                }
+            })
+    },
 
     joinChat: (req, res) => {
         const userID = req.body.user;
