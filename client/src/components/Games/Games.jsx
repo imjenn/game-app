4c99f067926f27145096cf575c985b9e662f4a37
Oscar Vaquera <vaqueraoscar0@gmail.com>
2022-02-13 15:26:37 -0800

    Search by category and game server list was added

--- client/src/components/Games/Games.jsx
+++ client/src/components/Games/Games.jsx
@@ -4,7 +4,10 @@ import styles from "./Games.module.css";
 import { Link, useParams } from 'react-router-dom';
 
 const Games = (props) => {
+    const user = JSON.parse(localStorage.getItem("User"));
     const [games, setGames] = useState([]);
+    const [category, setCategory] = useState('title');
+    const [gameFavorites,setGameFavorites] = useState([])
     const { id } = useParams();
 
     // Search result
@@ -18,33 +21,61 @@ const Games = (props) => {
                 setFoundGames(res.data);
             })
             .catch(err => console.log(err));
+
+        axios.get(`http://localhost:8000/findUser/${user}`)
+            .then(res => {
+                console.log(res.data)
+                setGameFavorites(res.data)
+            })
+            .catch(err => console.log(err));
     }, [])
 
     const filter = (e) => {
+        console.log(category)
         const keyword = e.target.value;
         console.log(keyword);
         if (keyword !== '') {
             const results = games.filter((game) => {
                 // Case sensitivity
-                return game.title.toLowerCase().includes(keyword.toLowerCase());
+                if(category === 'title'){
+                    return game.title.toLowerCase().includes(keyword.toLowerCase());
+                }else if (category === 'studio'){
+                    return game.studio.toLowerCase().includes(keyword.toLowerCase());
+                }else{
+                    return game.genre.toLowerCase().includes(keyword.toLowerCase());
+                }
             })
             setFoundGames(results);
         } else {
             // if field is empty, display all games
             setFoundGames(games);
-            console.log(foundGames);
         }
     }
 
     return (
         <div className={styles.games_container}>
             <div className={styles.games_side_nav}>
-                <ul>
-                    <li>This</li>
-                    <li>is</li>
-                    <li>a</li>
-                    <li>side</li>
-                    <li>nav</li>
+                <center>
+                    <br/><br/><select onChange={(e) => {setCategory(e.target.value);}}>
+                        <option value="title">Title</option>
+                        <option value="genre">Genre</option>
+                        <option value="studio">Studio</option>
+                    </select>
+                </center>
+
+                <br/><br/><br/><br/><ul>
+                    <h4>My Game Server</h4>
+                    {gameFavorites ? gameFavorites.map((games, idx) => {
+                        return (
+                            <Link className={styles.game} to={`/chatroom`}>
+                                <span className={styles.gameServerTitle}>{games.roomName}</span>
+                            </Link>
+                        )
+                    }) : null}
+
+                    <br/><br/>
+                    <h4>Game Favorites</h4>
+
                 </ul>
             </div>
             <div className={styles.games_section}>
