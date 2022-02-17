8a5c1793f03523bfeea5854820be979377f5b2b2
Jenn Nguyen <jennguy17@gmail.com>
2022-02-16 20:30:33 -0800

    added smooth scrolling

--- client/src/components/Forums/Forum.jsx
+++ client/src/components/Forums/Forum.jsx
@@ -13,6 +13,7 @@ const Forum = (props) => {
     const [forum, setForum] = useState({})
     const [game, setGame] = useState({})
     const [posts, setPosts] = useState({})
+    const [visible, setVisible] = useState(false); // Scroll to top
     const { id } = useParams();
 
 
@@ -41,75 +42,108 @@ const Forum = (props) => {
         element.innerText = parseInt(element.innerText, 10) - 1;
     }
 
+    // Display visibility of scroll to top button
+    const toggleScroll = () => {
+        let scrolled = document.documentElement.scrollTop;
+        if (scrolled > 400) {
+            setVisible(true);
+        } else if (scrolled <= 400) {
+            setVisible(false);
+        }
+    };
+
+    // Scroll to top on click
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    window.addEventListener('scroll', toggleScroll);
+
     return (
-        <div className={styles.forum_container}>
-            <div className={styles.forum_header}>
-                <img src={game.image} alt="" />
-                <h1>{game.title} Forum</h1>
-                <p>
-                    <span>About Community</span>
-                    {game.description}
-                </p>
-                <div className={styles.forum_buttons}>
-                    <Link className={styles.forum_link} to={`/post/new/${game._id}`}>Create Post</Link>
-                    <Link className={styles.forum_link} to={`/chatroom`}>Server</Link>
+        <>
+            <div className={styles.forum_container}>
+                <div className={styles.forum_header}>
+                    <img src={game.image} alt="" />
+                    <h1>{game.title} Forum</h1>
+                    <p>
+                        <span>About Community</span>
+                        {game.description}
+                    </p>
+                    <div className={styles.forum_buttons}>
+                        <Link className={styles.forum_link} to={`/post/new/${game._id}`}>Create Post</Link>
+                        <Link className={styles.forum_link} to={`/chatroom`}>Server</Link>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.forum_body}>
-                <div className={styles.forum_create_post}>
-                    <img src="https://pokecharms.com/data/attachment-files/2015/10/236933_Charmander_Picture.png" alt="" />
-                    <Link to={`/post/new/${game._id}`}>
-                        <input type="text" placeholder="Create Post" />
-                    </Link>
-                </div>
-                <div className={styles.forum_filter}>
-                    <p><FontAwesomeIcon icon={faFireFlameSimple} /> <span>Hot</span></p>
-                    <p><FontAwesomeIcon icon={faStar} /> <span>New</span></p>
-                    <p><FontAwesomeIcon icon={faChartLine} /> <span>Top</span></p>
-                </div>
-                {loaded ? posts.sort((a, b) => b.createdAt.localeCompare(a.createdAt)).map((p, idx) => {
-                    return (
-                        <div key={idx} className={styles.forum_post}>
-                            <div className={styles.forum_likes}>
-                                <FontAwesomeIcon
-                                    id={"like"}
-                                    icon={faArrowUp}
-                                    onClick={add}
-                                />
-                                <p id={"likes"} style={{ color: "white" }}>0</p>
-                                <FontAwesomeIcon
-                                    id={"dislike"}
-                                    icon={faArrowDown}
-                                    onClick={subtract}
-                                />
-                            </div>
-                            <div className={styles.forum_post_body}>
-                                <p>Posted by user {moment(p.createdAt).fromNow()}</p>
-                                <h3>{p.title}</h3>
-                                <div><Markup content={p.body} /></div>
-                                <div className={styles.post_icons}>
-                                    <p>
-                                        <FontAwesomeIcon icon={faMessage} />
-                                        <span>Comments</span>
-                                    </p>
-                                    <p>
-                                        <FontAwesomeIcon icon={faShare} />
-                                        <span>Share</span>
-                                    </p>
-                                    <p>
-                                        <FontAwesomeIcon icon={faBookmark} />
-                                        <span>Save</span>
-                                    </p>
-                                    <p>
-                                        <FontAwesomeIcon icon={faEllipsis} />
-                                    </p>
+                <div className={styles.forum_body}>
+                    <div className={styles.forum_create_post}>
+                        <img src="https://pokecharms.com/data/attachment-files/2015/10/236933_Charmander_Picture.png" alt="" />
+                        <Link to={`/post/new/${game._id}`}>
+                            <input type="text" placeholder="Create Post" />
+                        </Link>
+                    </div>
+                    <div className={styles.forum_filter}>
+                        <p><FontAwesomeIcon icon={faFireFlameSimple} /> <span>Hot</span></p>
+                        <p><FontAwesomeIcon icon={faStar} /> <span>New</span></p>
+                        <p><FontAwesomeIcon icon={faChartLine} /> <span>Top</span></p>
+                    </div>
+                    {loaded ? posts.sort((a, b) => b.createdAt.localeCompare(a.createdAt)).map((p, idx) => {
+                        return (
+                            <div key={idx} className={styles.forum_post}>
+                                <div className={styles.forum_likes}>
+                                    <FontAwesomeIcon
+                                        id={"like"}
+                                        icon={faArrowUp}
+                                        onClick={add}
+                                    />
+                                    <p id={"likes"} style={{ color: "white" }}>0</p>
+                                    <FontAwesomeIcon
+                                        id={"dislike"}
+                                        icon={faArrowDown}
+                                        onClick={subtract}
+                                    />
+                                </div>
+                                <div className={styles.forum_post_body}>
+                                    <p>Posted by user {moment(p.createdAt).fromNow()}</p>
+                                    <h3>{p.title}</h3>
+                                    <div><Markup content={p.body} /></div>
+                                    <div className={styles.post_icons}>
+                                        <p>
+                                            <FontAwesomeIcon icon={faMessage} />
+                                            <span>Comments</span>
+                                        </p>
+                                        <p>
+                                            <FontAwesomeIcon icon={faShare} />
+                                            <span>Share</span>
+                                        </p>
+                                        <p>
+                                            <FontAwesomeIcon icon={faBookmark} />
+                                            <span>Save</span>
+                                        </p>
+                                        <p>
+                                            <FontAwesomeIcon icon={faEllipsis} />
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                }) : null}
+                        )
+                    }) : null}
+                </div>
             </div>
-        </div>
+            <button
+                className={styles.scroll_to_top}
+                onClick={scrollToTop}
+                style={{
+                    display: visible
+                        ? "inline"
+                        : "none"
+                }}
+            >
+                Scroll to top
+             </button>
+        </>
     )
 }
 
